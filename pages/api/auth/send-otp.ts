7b78a06d35502eefd8fab0973050a2eff773aefa
22520592 <22520592@gm.uit.edu.vn>
2025-05-11 14:45:19 +0700

    authentication API

---
+++ pages/api/auth/send-otp.ts
@@ -0,0 +1,33 @@
+import { sendSMS } from "@/lib/actions/authentication.action";
+import corsMiddleware, {
+  authenticateToken,
+} from "@/middleware/auth-middleware";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  corsMiddleware(req, res, async () => {
+    if (req.method === "POST") {
+      const { phoneNumber } = req.body;
+
+      if (!phoneNumber) {
+        return res.status(400).json({ error: "Phone number is required" });
+      }
+
+      try {
+        const sendedOTP = await sendSMS(phoneNumber);
+        console.log(sendedOTP);
+
+        res.status(200).json(sendedOTP);
+      } catch (error) {
+        console.error("Error sending OTP:", error);
+        res.status(500).json({ error: "Failed to send OTP" });
+      }
+    } else {
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  });
+}
